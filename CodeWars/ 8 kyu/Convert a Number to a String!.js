bdd42bad4e03895583b345b1094b2e60fe4c9437
BukreevSergey <bukreevwork2@gmail.com>
2023-09-28 15:27:50 +0300

    Convert Number to String

---
+++ CodeWars/ 8 kyu/Convert a Number to a String!.js
@@ -0,0 +1,8 @@
+// We need a function that can transform a number (integer) into a string.
+// What ways of achieving this do you know?
+// Solution :
+
+function numberToString(num) {
+    return String(num);
+  }
+  console.log(numberToString(1,3,9))
