2487a8ef5fda190b235d12bfb83bed4c1f526075
BukreevSergey <bukreevwork2@gmail.com>
2023-10-09 15:29:03 +0300

    push from 10 to 1

---
+++ FreeCodeCamp/JavaScript Algoritms and Data Structures/Count Backwards With a For Loop.js
@@ -0,0 +1,10 @@
+// Push the odd numbers from 9 through 1 to myArray using a for loop.
+// SOLUTION :
+
+// Setup
+const myArray = [];
+
+// Only change code below this line
+for (let i = 9; i > 0; i -= 2) {
+  myArray.push(i);
+}
